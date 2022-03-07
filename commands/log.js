39177dc0b72d6ff67e0399a768a9459924e40f18
Omar Kamal <OmarQurashi868@gmail.com>
2022-03-07 22:12:29 +0400

    Added 3 commands

---
+++ commands/log.js
@@ -0,0 +1,14 @@
+module.exports = {
+  name: "log",
+  description: "Logs arguments...",
+  args: -1,
+  execute(message, args) {
+    if (!args[0].length === 0) {
+      message.channel.send(" ");
+    } else if (!args[0].startsWith(`"`) || !args[0].endsWith(`"`)) {
+      message.channel.send(`${args} is undefined.`);
+    } else {
+      message.channel.send(args[0].slice(0, -1).substring(1));
+    }
+  },
+};
