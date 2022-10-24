490f8cbc6d9a38b16fbf3cdfb753b66b693536e7
Omar Kamal <OmarQurashi868@gmail.com>
2022-10-25 01:09:36 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { Client, Intents, Collection, GatewayIntentBits } = require("discord.js");
 const fs = require("fs");
 const client = new Client({
-  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, GatewayIntentBits.MessageContent,],
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES,],
 });
 try {
   require("dotenv").config();
